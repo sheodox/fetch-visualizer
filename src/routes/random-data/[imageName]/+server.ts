ab86d006ea770a7bdb6cdf240e63e65c5888b092
sheodox <imagamer27@msn.com>
2023-05-25 19:44:44 -0500

    Use a different file path to images for prod

--- src/routes/random-data/[imageName]/+server.ts
+++ src/routes/random-data/[imageName]/+server.ts
@@ -4,14 +4,14 @@ import { error } from '@sveltejs/kit';
 import { images } from '$lib/images';
 import type { RequestHandler } from './$types';
 
+const photoPath = process.env.NODE_ENV === 'production' ? 'photos' : 'static/photos';
+
 export const GET = (async ({ params }) => {
 	if (!images.some((img) => img.fileName === params.imageName)) {
 		throw error(404);
 	}
 
-	const imageBuffer = await fs.readFile(
-		path.join(process.cwd(), 'static/photos', params.imageName)
-	);
+	const imageBuffer = await fs.readFile(path.join(process.cwd(), photoPath, params.imageName));
 
 	return new Response(imageBuffer);
 }) satisfies RequestHandler;
